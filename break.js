c7892d43a4e9a7e98f8d8826113e029b9363eb5d
vatiza <rakibur.mac@gmail.com>
2023-02-02 22:03:54 +0600

    add js

---
+++ break.js
@@ -0,0 +1,21 @@
+// for (var i = 1; i <= 10; i++){
+//     console.log(i)
+//     if (i > 5) {
+//         break;
+//     }
+// }
+// var roastGiven = 0;
+// while (roastGiven < 10) {
+//     console.log(roastGiven,'Roast dan', 'gift intem ansi');
+   
+//     roastGiven++;
+//     if (roastGiven > 4) {
+        
+//         break
+//     }
+// }
+var items=['aminur','rakib','mehedi', 'raihan','nahina','mohib'];
+for(var i=0;i<items.length;i++){
+     var item=items[i];
+     console.log(items)
+}
---
+++ while.js
@@ -0,0 +1,6 @@
+var roastGiven = 0;
+while (roastGiven <=7) {
+    console.log(roastGiven, 'Roast Done all')
+    console.log(roastGiven)
+    roastGiven ++;
+}
