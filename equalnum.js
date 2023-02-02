b039efaf330ec307bcf15ee86df8b07bbaf7e2b6
vatiza <rakibur.mac@gmail.com>
2023-02-03 01:57:50 +0600

    fix

--- equalnum.js
+++ equalnum.js
@@ -1,48 +1,47 @@
-// var numbers = [12, 43, 56, 32, 23, 67, 33, 45, 67, 80, 11, 34, 54, 67, 33];
-// for (var i = 0; i < numbers.length; i++){
-//     var number = numbers[i];
-//     if (number > 30) {
-//         continue;
-//     }
-//     console.log(number);
-// }
-// for (var i = 0; i <=10; i++){
-//     console.log(i);
-// }
-// console.log('reverse')
-// for (var i = 10; i > 1; i--){
-//     console.log(i);
-// }
+var numbers = [12, 43, 56, 32, 23, 67, 33, 45, 67, 80, 11, 34, 54, 67, 33];
+for (var i = 0; i < numbers.length; i++){
+    var number = numbers[i];
+    if (number > 30) {
+        continue;
+    }
+    console.log(number);
+}
+for (var i = 0; i <=10; i++){
+    console.log(i);
+}
+console.log('reverse')
+for (var i = 10; i > 1; i--){
+    console.log(i);
+}
 
-// var side1 = 9;
-// var side2 = 8;
-// var side3 = 93;
-// if (side1 == side2 || side1 == side3 || side2 == side3) {
-//     console.log('isosceles')
-// } else {
-//     console.log('not isosceles')
-// }
-// var i = 0;
-// for (i = 0; i < 5; i++){
+var side1 = 9;
+var side2 = 8;
+var side3 = 93;
+if (side1 == side2 || side1 == side3 || side2 == side3) {
+    console.log('isosceles')
+} else {
+    console.log('not isosceles')
+}
+var i = 0;
+for (i = 0; i < 5; i++){
     
-// };
-// console.log(i);
-// };
+};
+console.log(i);
 
-// var marks = [ 13, 15, 14, 20, 18];
-// for(var i=0; i<marks.length; i++){
-//   if(marks[i] >=15){
-//     continue
-//   }
-//     console.log(marks[i]);
-// };
-
-// var i = 5;
-// for (i; i < 5; i++) {
-//  console.log(i);
-// }
+var marks = [ 13, 15, 14, 20, 18];
+for(var i=0; i<marks.length; i++){
+  if(marks[i] >=15){
+    continue
+  }
+    console.log(marks[i]);
+};
 
 var i = 5;
-for (i < 5; i++){
-    console.log(i)
+for (i; i < 5; i++) {
+ console.log(i);
 }
+
+// var i = 5;
+// for (i < 5; i++){
+//     console.log(i)
+// }
