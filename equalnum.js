e21a5a54946fdfad9cbe1c3ef5d8309b9bdda154
vatiza <rakibur.mac@gmail.com>
2023-02-03 01:56:51 +0600

    slice

--- equalnum.js
+++ equalnum.js
@@ -14,11 +14,35 @@
 //     console.log(i);
 // }
 
-var side1 = 9;
-var side2 = 8;
-var side3 = 93;
-if (side1 == side2 || side1 == side3 || side2 == side3) {
-    console.log('isosceles')
-} else {
-    console.log('not isosceles')
+// var side1 = 9;
+// var side2 = 8;
+// var side3 = 93;
+// if (side1 == side2 || side1 == side3 || side2 == side3) {
+//     console.log('isosceles')
+// } else {
+//     console.log('not isosceles')
+// }
+// var i = 0;
+// for (i = 0; i < 5; i++){
+    
+// };
+// console.log(i);
+// };
+
+// var marks = [ 13, 15, 14, 20, 18];
+// for(var i=0; i<marks.length; i++){
+//   if(marks[i] >=15){
+//     continue
+//   }
+//     console.log(marks[i]);
+// };
+
+// var i = 5;
+// for (i; i < 5; i++) {
+//  console.log(i);
+// }
+
+var i = 5;
+for (i < 5; i++){
+    console.log(i)
 }
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,7 @@
-for (var i = 0; i <=10; i++){
-//     console.log(i);
-// }
+var marks = [ 13, 15, 14, 20, 18];
+// for(var i=0; i<marks.length; i++){
+//   if(marks[i] >=15){
+//     continue
+//   }
+//     console.log(marks[i]);
+// };
