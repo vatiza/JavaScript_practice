9675514cb458ab9398edd805d63538c370b3fccf
vatiza <rakibur.mac@gmail.com>
2023-02-04 23:22:05 +0600

    returnFunction

---
+++ js_function/retrunFunction.js
@@ -0,0 +1,9 @@
+function add(num1, num2) {
+    console.log(num1, num2);
+    var sum = num1 + num2;
+    // console.log(sum);
+    return sum;
+}
+
+var total = add(80, 20);
+console.log('total:',total)
