67674ca40c011e5f1e18ef8ce92d20e8757fb270
vatiza <rakibur.mac@gmail.com>
2023-02-07 01:45:58 +0600

    findOddSum

--- js_function/js_practice/findoddsum.js
+++ js_function/js_practice/findoddsum.js
@@ -1,3 +1,14 @@
 function getSum(numbers) {
-    
+    for (var i = 0; i < numbers.length; i++){
+        const index = i;
+        
+        const elements = numbers[index]
+        console.log(index,elements)
+    }
+   
+
 }
+
+const myNumbers=[12,45,123,47,86,45,13,456]
+
+getSum(myNumbers)
