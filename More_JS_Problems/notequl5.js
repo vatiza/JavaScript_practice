6e2fb9eeb7535cafedec94af3726cf573383a3ad
vatiza <rakibur.mac@gmail.com>
2023-02-10 06:14:51 +0600

    ad js

---
+++ More_JS_Problems/notequl5.js
@@ -0,0 +1,12 @@
+// let i=5
+// if (i !=5) {
+//     console.log('not equal')
+// }
+// else {
+//     console.log('equal')
+// }
+let sum = 0; 
+for( let i = 0; i<=3;i++){ 
+sum = sum + i; 
+}
+console.log(sum)
--- More_JS_Problems/validDation.js
+++ More_JS_Problems/validDation.js
@@ -9,3 +9,10 @@ function add(num1, num2) {
 }
 const result = add(12, '45');
 console.log(result)
+function multiply(num1, num2) {
+    console.log(num1,num2)
+    return num1 * num2;
+
+}
+const output = multiply(12 , 45);
+console.log(output);
